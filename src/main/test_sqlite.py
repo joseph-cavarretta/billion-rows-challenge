67d035063a1ba326acccf97371acb3a9b6ad8607
joseph-cavarretta <josephmc241@gmail.com>
2024-03-24 14:08:43 -0600

    finished sqlite module

--- src/main/test_sqlite.py
+++ src/main/test_sqlite.py
@@ -1,17 +1,39 @@
+import os
+import time
 import subprocess
+from pathlib import Path
 import sqlite3 as sql
 
-DB_PATH = '../sqlite3/'
-DB_NAME = 'stations.db'
-DATA = '../data/stations_test.txt'
+DB_PATH = Path('../sqlite3/stations.db').resolve()
+DATA = Path('../data/stations_test.txt').resolve()
 TABLE = 'stations'
 COL_1_NAME = 'station'
 COL_1_TYPE = 'TEXT'
 COL_2_NAME = 'reading'
 COL_2_TYPE = 'INTEGER'
 
+
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        t1 = time.perf_counter()
+        res = func(*args, **kwargs)
+        t2 = time.perf_counter()
+        print(f'{func.__name__}() runtime: {(t2 - t1):.4f} seconds')
+        return res
+    return wrapper
+
+
+def create_db():
+    # if db already exists, remove for testing only
+    if os.path.isfile(DB_PATH):
+        os.remove(DB_PATH)
+    # create new instance of db
+    with open(DB_PATH, 'w'): pass
+    print(f'DB created at {str(DB_PATH)}')
+
+
 def connect_db():
-    conn = sql.connect(DB_PATH + DB_NAME)
+    conn = sql.connect(DB_PATH)
     return conn
 
 
@@ -19,27 +41,56 @@ def create_table(conn):
     # drop table first?
     ddl = f"""
         CREATE TABLE IF NOT EXISTS {TABLE} (
-            {COL_1_NAME} {COL_1_TYPE} PRIMARY KEY, 
+            {COL_1_NAME} {COL_1_TYPE}, 
             {COL_2_NAME} {COL_2_TYPE}
         )"""
     
     conn.execute(ddl)
+    print(f'Table {TABLE} created')
 
 
-def load_db():
-    result = subprocess.run(
+def load_db(conn):
+    stdout = subprocess.run(
         [
             'sqlite3',
-            DB_NAME,
+            str(DB_PATH),
             '-cmd',
             '.mode csv',
             '.separator ;',
-            '.import --skip 1 ' + DATA + ' ' + TABLE
+            '.import ' + str(DATA) + ' ' + TABLE
         ],
     capture_output=True
     )
+    row_count = get_row_count(conn)
+    print(f'Table {TABLE} loaded with {row_count} rows')
 
 
+def get_row_count(conn):
+    query = f"""
+        SELECT COUNT(1) FROM {TABLE}
+    """
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    return results[0][0]
+
+@timeit
 def test_sqlite(conn):
-    pass
+    query = f"""
+        SELECT station, ROUND(AVG(reading),2) , MIN(reading), MAX(reading)
+        FROM {TABLE}
+        GROUP BY station
+        ORDER BY station
+    """
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    print(*results, sep='\n')
 
+
+if __name__ == '__main__':
+    create_db()
+    conn = connect_db()
+    create_table(conn)
+    load_db(conn)
+    test_sqlite(conn)
