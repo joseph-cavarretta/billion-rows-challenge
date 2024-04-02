db20ea2a78274d946007956268c0476b3b54143d
joseph-cavarretta <josephmc241@gmail.com>
2024-04-01 21:39:25 -0600

    python module added

--- src/main/test_python.py
+++ src/main/test_python.py
@@ -1,20 +1,3 @@
-# read file in chuncks and multithread each to a processor
-# for station in stations:
-# station.readings_cnt = src + current
-# station.readings_sum = srs + current
-# station.min = curr if curr < min else min
-# station.max = curr if curr > max else max
-
-# try:
-#     item = station_measurements[station]
-#     item[0] = min(item[0], min_)
-#     item[1] = max(item[1], max_)
-#     item[2] += sum_
-#     item[3] += count
-# except KeyError: # first time entering it 
-#     station_measurements[station] = [min_, max_, sum_, count]
-
-import os
 import time
 import subprocess
 from pathlib import Path
